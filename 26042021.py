ad78ec52976d4ad760aa338193c63275f237e9de
IOTNOUT8\СТУДЕНТ <gherlghag@mail.ru>
2021-04-26 15:02:25 +0200

    26.04.2021

---
+++ 26042021.py
@@ -0,0 +1,33 @@
+a = [0] * 50
+b = [1]
+c = [1]
+print(b + a + c)
+
+a1 = [0, 1] * 10
+print(a1)
+
+mass = []
+for i in range(1, 10, 2):
+	mass.append(i)
+print(mass)
+
+
+mass = []
+
+x = int(input('введите первый элемент - '))
+d = int(input('введите разность -'))
+
+for i in range(x, x+100, d):
+	mass.append(i)
+print(mass)
+
+
+mass = []
+for i in range (99, 0, -3):
+	mass.append(i)
+print(mass)
+
+
+mass = []
+for i in range (1, 100):
+		
